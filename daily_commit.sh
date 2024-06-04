c5dd859e53ea4bcc0a5f0a64e7d769a44f594692
sourabh@32 <sourabhsharma181003@gmail.com>
2024-06-04 13:09:27 +0530

    automated commit

--- daily_commit.sh
+++ daily_commit.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-cd ~
-cd projects
-cd automated-scripts
+
 git add .
 git commit -m "automated commit"
-git push origin master
+git push origin main:wq
 # Print a message indicating the process is complete
 echo "Changes have been committed and pushed to GitHub on branch $BRANCH_NAME."
 
